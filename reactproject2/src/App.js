5a18f75dff38cb34f58739fe19c78acbc9db6541
jayson <jaysonfinny93@gmail.com>
2021-01-03 20:04:07 +0530

    Assignment 1

---
+++ reactproject2/src/App.js
@@ -0,0 +1,24 @@
+import React,{Component} from 'react';
+import Main from './Components/MainComponents.js'; 
+import { BrowserRouter } from 'react-router-dom';
+import './App.css';
+import { Provider } from 'react-redux';
+import { ConfigureStore } from './redux/configureStore';
+
+const store = ConfigureStore();
+
+class App extends Component{
+
+  render(){
+  return (
+    <Provider store={store}>
+        <BrowserRouter>
+          <div className="App">
+            <Main />
+          </div>
+        </BrowserRouter>
+      </Provider>
+  );
+}}
+
+export default App;
